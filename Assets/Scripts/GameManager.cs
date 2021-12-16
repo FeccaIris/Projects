eff8a2439a39749f1b66c4c8438b4c0aa5c4fc07
FeccaIris <slsls70094@gmail.com>
2021-12-16 15:04:50 +0900

    12.16

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,10 +8,21 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnEnemy()
     {
-        yield return new WaitForSeconds(30.0f);
-        GameObject pref =  Resources.Load("Enemy1") as GameObject;
-        GameObject go = Instantiate(pref);
-        go.transform.position = _player.transform.position * 2;
+        while (true)
+        {
+            yield return new WaitForSeconds(3.0f);
+
+            GameObject pref =  Resources.Load("Enemy1") as GameObject;
+            GameObject go = Instantiate(pref);
+
+            float start = 0f;
+            float end = 10f;
+            float x = Random.Range(start, end);
+            float y = Random.Range(start, end);
+
+            go.transform.position = new Vector3(x, y, 0);
+        }
+
     }
 
 

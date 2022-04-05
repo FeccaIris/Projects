45ee98c67c20f2cd4b78998a4b9a9e1bb46170ca
FeccaIris <slsls70094@gmail.com>
2022-04-06 03:12:27 +0900

    0405

--- Assets/2D_Survival/Scripts/Game/Canvas/Playing/PopUp/LevelUp/Tab/SecondTab.cs
+++ Assets/2D_Survival/Scripts/Game/Canvas/Playing/PopUp/LevelUp/Tab/SecondTab.cs
@@ -79,5 +79,94 @@ namespace SV
 
             
         }
+        public void Test2(PlayerSkill ps)
+        {
+            List<Category> cat = new List<Category> { Category.COOL, Category.DAMAGE, Category.INTERVAL };
+
+            if (ps != null)
+            {
+                if (ps._isProjectile == true)
+                {
+                    cat.Add(Category.PIERCE);
+                    cat.Add(Category.SPEED);
+                    cat.Add(Category.EA);
+                }
+                else
+                {
+                    cat.Add(Category.MAINTAIN);
+                }
+            }
+
+            int r1 = Random.Range(0, cat.Count);
+            int r2, r3;
+
+            while (true)
+            {
+                r2 = Random.Range(0, cat.Count);
+                if (r1 != r2) break;
+            }
+            while (true)
+            {
+                r3 = Random.Range(0, cat.Count);
+                if (r3 != r2 && r3 != r1) break;
+            }
+
+            Category c1, c2, c3;
+            c1 = cat[r1];
+            c2 = cat[r2];
+            c3 = cat[r3];
+            List<Category> list = new List<Category> { c1, c2, c3 };
+
+            for (int i = 0; i < _buttons.Count; i++)
+            {
+                _buttons[i]._cat = list[i];
+
+                Text t = _buttons[i].transform.Find("Text").GetComponent<Text>();
+                //t.text = list[i].ToString();
+                switch (list[i])
+                {
+                    case Category.DAMAGE:
+                        {
+                            t.text = "공격력 증가";
+                            break;
+                        }
+                    case Category.COOL:
+                        {
+                            t.text = "쿨타임 감소";
+                            break;
+                        }
+                    case Category.EA:
+                        {
+                            t.text = "개수 증가";
+                            break;
+                        }
+                    case Category.INTERVAL:
+                        {
+                            t.text = "피해간격 감소";
+                            break;
+                        }
+                    case Category.MAINTAIN:
+                        {
+                            t.text = "유지시간 증가";
+                            break;
+                        }
+                    case Category.PIERCE:
+                        {
+                            t.text = "관통력 증가";
+                            break;
+                        }
+                    case Category.REACH:
+                        {
+                            t.text = "사거리 증가";
+                            break;
+                        }
+                    case Category.SPEED:
+                        {
+                            t.text = "속도 증가";
+                            break;
+                        }
+                }
+            }
+        }
     }
 }
